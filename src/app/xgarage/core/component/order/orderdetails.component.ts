9951fcdbf6cb30d8ff3f4ffc4b77f429f0eac105
fatima.ahmed <fatima.ahmed@intelligentprojects.net>
2023-02-27 16:29:33 +0400

    add order send email api

--- src/app/xgarage/core/component/order/orderdetails.component.ts
+++ src/app/xgarage/core/component/order/orderdetails.component.ts
@@ -8,6 +8,9 @@ import { DialogService } from 'primeng/dynamicdialog';
 import { DataService } from 'src/app/xgarage/common/generic/dataservice';
 import { DatePipe } from '@angular/common';
 import domtoimage from 'dom-to-image';
+import jsPDF from 'jspdf';
+import { OrderService } from '../../service/order.service';
+import { Status } from 'src/app/xgarage/common/model/status';
 @Component({
     selector: 'order-details',
     templateUrl: './orderdetails.component.html',
@@ -34,7 +37,7 @@ import domtoimage from 'dom-to-image';
 })
 export class OrderDetailsComponent extends GenericDetailsComponent implements OnInit {
 
-    constructor(public route: ActivatedRoute, private dialogService: DialogService, public router: Router, private dataService: DataService<any>, public messageService: MessageService, public confirmService: ConfirmationService,
+    constructor(private orderService: OrderService, public route: ActivatedRoute, private dialogService: DialogService, public router: Router, private dataService: DataService<any>, public messageService: MessageService, public confirmService: ConfirmationService,
         public breadcrumbService: AppBreadcrumbService, public datePipe: DatePipe) {
         super(route, router, null, datePipe, null, breadcrumbService);
     }
@@ -48,9 +51,7 @@ export class OrderDetailsComponent extends GenericDetailsComponent implements On
     ngOnInit() {
         this.master = JSON.parse(localStorage.getItem('orderData'));
         this.order = JSON.parse(localStorage.getItem('order'));
-
-        console.log(this.master, this.order)
-
+        console.log(this.master,this.order)
         this.master.forEach(element => {
             this.totalVat = this.totalVat + element.vat;
         });
@@ -68,6 +69,8 @@ export class OrderDetailsComponent extends GenericDetailsComponent implements On
             { field: 'price', header: 'LINE VALUE' }
         ];
 
+         this.initActionMenu();
+
     }
 
     getPdf() {
@@ -84,6 +87,82 @@ export class OrderDetailsComponent extends GenericDetailsComponent implements On
             .catch(function (error) {
                 console.error('oops, something went wrong!', error);
             });
+    }
+
+    downloadPDF() {
+
+        var node = this.invoice.nativeElement;
+
+        var img;
+        var filename;
+        var newImage;
+
+
+        domtoimage.toPng(node, { bgcolor: '#fff' })
+
+            .then((dataUrl) => {
+
+                img = new Image();
+                img.src = dataUrl;
+                newImage = img.src;
+
+                img.onload = () => {
+
+                    var pdfWidth = img.width;
+                    var pdfHeight = img.height;
+
+                    // FileSaver.saveAs(dataUrl, 'my-pdfimage.png'); // Save as Image
+
+                    var doc;
+
+                    if (pdfWidth > pdfHeight) {
+                        doc = new jsPDF('l', 'px', [pdfWidth, pdfHeight]);
+                    }
+                    else {
+                        doc = new jsPDF('p', 'px', [pdfWidth, pdfHeight]);
+                    }
+
+
+                    var width = doc.internal.pageSize.getWidth();
+                    var height = doc.internal.pageSize.getHeight();
+
+                    //download pdf
+                    doc.addImage(newImage, 'PNG', 10, 10, width, height);
+                    filename = 'order _' + '.pdf';
+                    //doc.save(filename);
+
+                    //send pdf to the server
+                    var blob = doc.output('blob');
+                    var formData = new FormData();
+
+                    let req = { "orderId": this.order.id, "lpo": blob};
+
+                    for (var key in req) {
+                        formData.append(key, req[key]);
+                    }
+
+                    this.orderService.notify(formData).subscribe(res => {
+                        console.log(res)
+                    }, err => {
+                        console.log(err)
+                    })
+                };
+
+
+            })
+            .catch(function (error) {
+                // Error Handling
+            });
+    }
+
+    initActionMenu() {
+        this.menuItems = [
+            {
+                label: 'Send Email', icon: 'pi pi-email', command: () => {
+                    this.downloadPDF()
+                }
+            }
+        ];
     }
 
 }
--- src/app/xgarage/core/service/order.service.ts
+++ src/app/xgarage/core/service/order.service.ts
@@ -11,4 +11,8 @@ export class OrderService extends GenericService<any> {
     constructor(protected http: HttpClient) {
         super(http, config.storeApiUrl + '/orders');
     }
+
+    notify(order) {
+        return this.http.post(this.apiServerUrl + '/notify', order);
+    }
 }
