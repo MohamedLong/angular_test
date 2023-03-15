c628e6413048c77f29289b619cc5cf66e8e2bb0a
Intelligentprojects <86415222+IntelligetProjects@users.noreply.github.com>
2023-03-15 17:40:09 +0400

    resolve conflicts

--- src/app/xgarage/core/component/job/jobdetails.component.ts
+++ src/app/xgarage/core/component/job/jobdetails.component.ts
@@ -27,7 +27,7 @@ import html2canvas from 'html2canvas';
 @Component({
     selector: 'job-details',
     templateUrl: './jobdetails.component.html',
-    // styleUrls: ['../../../../demo/view/tabledemo.scss'],
+    styleUrls: ['../../../../demo/view/tabledemo.scss'],
     styles: [`
         :host ::ng-deep .p-dialog .product-image {
             width: 150px;
@@ -87,8 +87,8 @@ export class JobDetailsComponent extends GenericDetailsComponent implements OnIn
     jobDto: UpdateJobDto = {};
     modalPart: any = [];
     displayModal: boolean = false;
-    displayNotIntrestedSuppliers: boolean = false;
-    NotIntrestedSuppliers: any[] = [];
+    displayNotInterestedSuppliers: boolean = false;
+    notInterestedSuppliers: any[] = [];
 
     constructor(public route: ActivatedRoute, private jobService: JobService, private requestService: RequestService, public router: Router, public messageService: MessageService, public confirmService: ConfirmationService, private cd: ChangeDetectorRef,
         public breadcrumbService: AppBreadcrumbService, private bidService: BidService, public datePipe: DatePipe, public statusService: StatusService, private authService: AuthService) {
@@ -96,16 +96,11 @@ export class JobDetailsComponent extends GenericDetailsComponent implements OnIn
     }
 
     ngOnInit() {
-
-        // if (this.route.snapshot.queryParams['id']) {
-        //     localStorage.setItem('jobId', this.route.snapshot.queryParams['id']);
-        //     this.router.navigate(
-        //         ['job-details'],
-        //         { relativeTo: this.route, queryParams: {} }
-        //     );
-        // }
-
-         if (localStorage.getItem('jobId')) {
+        //console.log('init')
+        console.log(localStorage.getItem('jobId'));
+        if (this.route.snapshot.queryParams['id']) {
+            this.getJobObject(this.route.snapshot.queryParams['id']);
+        } else if (localStorage.getItem('jobId')) {
             this.getJobObject(JSON.parse(localStorage.getItem('jobId')));
         }
 
@@ -120,7 +115,7 @@ export class JobDetailsComponent extends GenericDetailsComponent implements OnIn
         this.jobService.getById(id).subscribe(
             {
                 next: (data) => {
-                    //console.log(data)
+                    console.log(data)
                     this.master = data;
                     this.master.claimNo = data.claimNo;
                     this.masters.push(this.master);
@@ -317,7 +312,7 @@ export class JobDetailsComponent extends GenericDetailsComponent implements OnIn
     }
 
     viewBidsByRequest(request: Request) {
-        //console.log('request:',this.bidDtos)
+    //console.log('request:',this.bidDtos)
         this.originalBidList = this.bidDtos;
         this.partName = request.part.name;
         this.selection = 'single';
@@ -343,10 +338,9 @@ export class JobDetailsComponent extends GenericDetailsComponent implements OnIn
 
     viewNotInterestedSuppliers(id: number) {
         this.requestService.getNotInterestedSuppliers(id).subscribe(res => {
-            console.log(res)
             if(res.length > 0) {
-                this.NotIntrestedSuppliers = res;
-                this.displayNotIntrestedSuppliers = true;
+                this.notInterestedSuppliers = res;
+                this.displayNotInterestedSuppliers = true;
             } else {
                 this.messageService.add({ severity: 'info', summary: 'There is no not-interested suppliers.' });
             }
@@ -542,13 +536,13 @@ export class JobDetailsComponent extends GenericDetailsComponent implements OnIn
     downloadPdf() {
         let DATA: any = this.bidsTable.nativeElement;
         html2canvas(DATA).then((canvas) => {
-            let fileWidth = 208;
-            let fileHeight = (canvas.height * fileWidth) / canvas.width;
-            const FILEURI = canvas.toDataURL('image/png');
-            let PDF = new jsPDF('p', 'mm', 'a4');
-            let position = 0;
-            PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
-            PDF.save('bids.pdf');
+          let fileWidth = 208;
+          let fileHeight = (canvas.height * fileWidth) / canvas.width;
+          const FILEURI = canvas.toDataURL('image/png');
+          let PDF = new jsPDF('p', 'mm', 'a4');
+          let position = 0;
+          PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+          PDF.save('bids.pdf');
         });
     }
 
@@ -613,7 +607,7 @@ export class JobDetailsComponent extends GenericDetailsComponent implements OnIn
     }
 
     getValueAfterVat(price: number, vat: number, discount: number) {
-        return price + ((price - discount) * vat) / 100;
+        return price + ((price - discount) * vat)/100;
     }
 
     rejectMultipleBids() {
