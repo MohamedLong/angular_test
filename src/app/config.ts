bd983cb1b603c7175799c8b9522640c63a9f0fe6
fatima.ahmed <fatima.ahmed@intelligentprojects.net>
2023-05-08 17:04:25 +0400

    update token interceptor to skip ssl verifiaction (temp)

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,10 +1,12 @@
 export const config = {
-    apiUrl: 'http://letsgo-oman.com:6060/api',
-    coreApiUrl: 'http://letsgo-oman.com:6060/core/api/v1',
-    storeApiUrl: 'http://letsgo-oman.com:6060/store/api/v1',
-    purchaseApiUrl: 'http://letsgo-oman.com:3030',
-    dashboardUrl: 'http://letsgo-oman.com:6060/api/v1/dashboard',
+    apiUrl: 'https://144.217.243.3:6060/api',
+    coreApiUrl: 'https://144.217.243.3:6060/core/api/v1',
+    storeApiUrl: 'https://144.217.243.3:6060/store/api/v1',
+    purchaseApiUrl: 'https://letsgo-oman.com:3030',
+    dashboardUrl: 'https://144.217.243.3:6060/api/v1/dashboard',
   };
 
 
   //https://xgarage.om:6060
+  //144.217.243.3:6060
+  //http://letsgo-oman.com:6060
