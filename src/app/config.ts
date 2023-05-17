e19e79c0ddd73688510c73d077277e797359d4c0
fatima.ahmed <fatima.ahmed@intelligentprojects.net>
2023-05-17 11:05:12 +0400

    update test endpint in config

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,9 +1,9 @@
 export const config = {
-    apiUrl: 'https://144.217.243.3:6060/api',
-    coreApiUrl: 'https://144.217.243.3:6060/core/api/v1',
-    storeApiUrl: 'https://144.217.243.3:6060/store/api/v1',
+    apiUrl: 'http://letsgo-oman.com:6060/api',
+    coreApiUrl: 'http://letsgo-oman.com:6060/core/api/v1',
+    storeApiUrl: 'http://letsgo-oman.com:6060/store/api/v1',
     purchaseApiUrl: 'https://letsgo-oman.com:3030',
-    dashboardUrl: 'https://144.217.243.3:6060/api/v1/dashboard',
+    dashboardUrl: 'http://letsgo-oman.com:6060/api/v1/dashboard',
   };
 
 
